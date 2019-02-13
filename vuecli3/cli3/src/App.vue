d1238ecc9661512fc3f926defb35a66efdafa16b
chenchenyi <2814871696@qq.com>
2019-02-13 14:24:07 +0800

    vd

---
+++ vuecli3/cli3/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link :to="{name:'mine',params:{name:'zs',id:'001'}}">Mine</router-link> |
+      <router-link to="/test">Test</router-link>
+      
+    </div>
+    <transition name="fade" mode="'out-in'">
+    <router-view/>
+    </transition>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.fade-enter{
+  transform: translateX(-100%);
+}
+.fade-enter-active{
+  transition: transform 1s ease;
+}
+.fade-enter-to{
+transform: translateX(0);
+}
+.fade-leave{
+  opacity: 1;
+}
+.fade-leave-active{
+  transition: opacity 1s ease;
+}
+.fade-leave-to{
+  opacity: 0;
+}
+
+</style>
---
+++ vuecli3/cli3/src/views/Mine.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        我是Mine<br>
+        我的名字是{{$route.params.name}}
+        -----
+       我的学号是{{$route.params.id}}
+       <br>
+       {{$store.state.count}}
+       <button @click="$store.commit('add',10)">加十</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
